958a5de03f42740dd40d2c6b41fd16947d9f8b85
avinash.yeccaluri@tyfone.com <avinash.yeccaluri@tyfone.com>
2020-02-06 20:04:10 +0530

    towards-final

--- JS/effects.js
+++ JS/effects.js
@@ -1,14 +1,40 @@
+let imgArr = [
+  '../IMG/action-adult-affection-eldery-339620.jpg',
+  '../IMG/activity-asian-people-boys-children-1153976.jpg',
+  '../IMG/adult-doctors-gloves-health-263337.jpg',
+];
+
 var myIndex = 0;
 carousel();
 
 function carousel() {
-  var i;
-  var x = document.getElementsByClassName("img-trail");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 3000); // Change image every 2 seconds
+  // var i;
+  // var x = document.getElementsByClassName("image_container");
+  // for (i = 0; i < x.length; i++) {
+  //   x[i].style.display = "none";  
+  // }
+  // myIndex++;
+  // if (myIndex > x.length) {myIndex = 1}    
+  // x[myIndex-1].style.display = "block";  
+  // setTimeout(carousel, 3000); // Change image every 2 seconds
+
+let imgContainer = document.getElementsByClassName('image_container')[0];
+console.log(imgContainer.firstChild);
+
+  setInterval(() => {
+    imgContainer.innerHTML = '';
+    let imgPath = imgArr[myIndex++];
+    let theImg = document.createElement('img');
+    theImg.setAttribute('src',imgPath);
+    theImg.setAttribute('alt','hmmm');
+    theImg.setAttribute('class','the_image');
+    imgContainer.appendChild(theImg);
+
+    if(myIndex >= imgArr.length){
+      myIndex = 0;
+    }
+  }, 4000);
+
 }
+
+
