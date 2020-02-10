4e39b547202dc0c20338c37c576d93717b9b01e8
vinaypn16 <vinaypn16@gmail.com>
2020-02-10 09:39:26 +0530

    responsive progress

--- JS/effects.js
+++ JS/effects.js
@@ -35,3 +35,16 @@
 //         }
 //     }, 4000);
 // }
+
+function myFunction() {
+    let x = document.getElementsByClassName("navBarContainer");
+    console.log(x);
+
+    for (let i = 0; i < x.length; i++) {
+        if (x[i].style.display === "block") {
+            x[i].style.display = "none";
+        } else {
+            x[i].style.display = "block";
+        }
+    }
+}
